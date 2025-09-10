755373aede960efd36b5d22284e60e791bde6a2c
kofi arhin <kofiarhin69@gmail.com>
2025-09-10 21:54:25 +0100

    restructured the urls

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const fetchYouTubeDataForChannels = require("./src/utils/fetchYouTubeDataForChan
 
 app.get("/", async (req, res, next) => {
   const result = await fetchYouTubeDataForChannels(
-    ["https://www.youtube.com/@HeadlessYouTuber"],
+    [
+      "https://www.youtube.com/@devkofi",
+      "https://www.youtube.com/@ThePrimeTimeagen",
+      "https://www.youtube.com/@t3dotgg",
+      "https://www.youtube.com/@TraversyMedia",
+    ],
     { limit: 600, debug: true }
   );
   return res.json(result);
